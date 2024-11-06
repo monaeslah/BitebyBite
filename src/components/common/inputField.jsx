6eb93aa21ba508847b858ac56322a68af225e6df
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-11-06 10:27:22 +0100

    update input

--- src/components/common/inputField.jsx
+++ src/components/common/inputField.jsx
@@ -1,10 +1,12 @@
-const InputField = ({ className = '', label, icon, children }) => {
+const InputField = ({ className = '', label, icon = '', children }) => {
   return (
     <div className='inputContainer'>
       {label && <p className='form-field'>{label}</p>}
       <div className={`input-wrapper ${className}`}>
-        {icon && <img src={icon} alt='Input icon' className='icon-before' />}
-        {children}
+        {icon ? (
+          <img src={icon} alt='Input icon' className='icon-before' />
+        ) : null}
+        {children || <span>No content provided</span>}
       </div>
     </div>
   )
