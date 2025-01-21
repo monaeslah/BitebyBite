e43be4e281fa528ee66d60a7bf79306d24a2881c
moonae-tech <eslahkonha.mona@gmail.com>
2025-01-21 15:03:08 +0100

    update searchbar postion

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,23 +1,19 @@
 import InputField from './common/inputField'
 const SearchBar = ({ query, setQuery }) => {
   return (
-    <div>
-      <div>
-        <div>
-          <span>Search</span>
-        </div>
-        <InputField
-          className='inputField xlargeInput'
-          // label={field.charAt(0).toUpperCase() + field.slice(1)}
-        >
-          <input
-            type='text'
-            className='search-bar'
-            value={query}
-            onChange={e => setQuery(e.target.value)}
-          />
-        </InputField>
-      </div>
+    <div id='searchbar'>
+      <InputField
+        className='inputField xlargeInput'
+        // label={field.charAt(0).toUpperCase() + field.slice(1)}
+      >
+        <input
+          type='text'
+          className='search-bar'
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder='search your food name here '
+        />
+      </InputField>
     </div>
   )
 }
